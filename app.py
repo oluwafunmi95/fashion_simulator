0c29510fa3cbfaf313714b2bc32aec3a305e6e62
Oluwafunmilayo <olumoko.oluwafunmilayo@gmail.com>
2025-01-06 16:02:22 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+import os
+
+app = Flask(__name__)
+
+# Sample outfit data
+outfits = [
+    {"name": "Red Dress", "image": "static/images/red-dress.png"},
+    {"name": "Blue Dress", "image": "static/images/blue-dress.png"},
+    {"name": "Yellow Dress", "image": "static/images/yellow-dress.png"},
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html", outfits=outfits)
+
+@app.route("/try-on", methods=["POST"])
+def try_on():
+    data = request.json
+    selected_outfit = data.get("outfit")
+    return jsonify({"message": "Outfit selected!", "outfit": selected_outfit})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
---
+++ test.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Sample outfit data
+outfits = [
+    {"name": "Red Dress", "image": "static/images/red-dress.png"},
+    {"name": "Blue Jeans", "image": "static/images/blue-jeans.png"},
+    {"name": "Green Shirt", "image": "static/images/green-shirt.png"},
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html", outfits=outfits)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+
+    
