796fe9a12a550ee2f6166d93e2695bbc47d709ae
Oluwafunmilayo <olumoko.oluwafunmilayo@gmail.com>
2025-01-17 11:54:53 +0100

    Initial commit

--- test.py
+++ test.py
@@ -14,7 +14,4 @@ def home():
     return render_template("index.html", outfits=outfits)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
-
-
-    
+    app.run(debug=True)
